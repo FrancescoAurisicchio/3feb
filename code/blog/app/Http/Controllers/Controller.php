99187435c91ea0ad1adb9c06077504061021e873
Francesco <francescoaurisicchio9@gmail.com>
2021-02-03 17:40:15 +0100

    2.0

--- code/blog/app/Http/Controllers/Controller.php
+++ code/blog/app/Http/Controllers/Controller.php
@@ -6,18 +6,17 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+protected function respondWithToken($token)
+{
+    return response()->json([
+        'token' => $token,
+        'token_type' => 'bearer',
+        'expires_in' => Auth::factory()->getTTL() * 60
+    ], 200);
+}
 }
 //import auth facades
 use Illuminate\Support\Facades\Auth;
 
 
-//Add this method to the Controller class
-protected function respondWithToken($token)
-  {
-      return response()->json([
-          'token' => $token,
-          'token_type' => 'bearer',
-          'expires_in' => Auth::factory()->getTTL() * 60
-      ], 200);
-  }
+
--- code/blog/routes/web.php
+++ code/blog/routes/web.php
@@ -29,3 +29,20 @@ $router->group(['prefix' => 'api'], function () use ($router) {
   // Matches "/api/login
   $router->post('login', 'AuthController@login');
  });
+ // API route group
+$router->group(['prefix' => 'api'], function () use ($router) {
+    // Matches "/api/register
+   $router->post('register', 'AuthController@register');
+     // Matches "/api/login
+    $router->post('login', 'AuthController@login');
+
+    // Matches "/api/profile
+    $router->get('profile', 'UserController@profile');
+
+    // Matches "/api/users/1 
+    //get one user by id
+    $router->get('users/{id}', 'UserController@singleUser');
+
+    // Matches "/api/users
+    $router->get('users', 'UserController@allUsers');
+});
